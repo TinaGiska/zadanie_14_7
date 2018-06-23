bf5294f79f7d6603791b032d11afa84929056254
Tina Giska <tina.tylicka@gmail.com>
2018-06-23 19:53:16 +0200

    Update task

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,19 +5,20 @@ var Contact = React.createClass({
 
   render: function() {
     return (
-      <div className={'contactItem'},
-        <img className: {'contactImage'}
+      <div className='contactItem'>
+        <img className='contactImage'
           src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
-        <p className={'contactLabel'}>
+        <p className='contactLabel'>
             Imię : {this.props.contact.firstName}
         </p>
-        <p className={'contactLabel'}>
+        <p className='contactLabel'>
         Nazwisko : {this.props.contact.lastName}
         </p>
         <a href={'mailto:' + this.props.item.email}>
           {this.props.item.email}
         </a>
+      </div>
 
 )
 }
-})
+});
